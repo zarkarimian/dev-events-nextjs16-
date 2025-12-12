9de8006530ce75bd3ad42f394d8d58c3b01d1997
zar <zark7697@gmail.com>
2025-12-12 18:35:47 +0330

    Button&Event

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
+import LightRays from "@/components/Light";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibasted-grotesk",
@@ -25,9 +26,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${schibstedGrotesk .variable} ${martianMono.variable} antialiased`}
+        className={`${schibstedGrotesk.variable} ${martianMono.variable} antialiased`}
       >
-        {children}
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0.0}
+            distortion={0.01}
+            className="custom-rays"
+          />
+        </div>
+        <main>{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
 const page = () => {
-  return <h1>Welcome to Next.js 16!</h1>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Event</h3>
+        <ul className="events">{[1, 2, 3, 4, 5].map((event) =>(<li key={event}>Event {event}</li>))}</ul>
+
+      </div>
+    </section>
+  );
 };
 
 export default page;
